0fdec8a1a7d0f07155b93e0df424f50cb3644263
huzaifa-at-214759062006 <ahmedhuzaifa184@gmail.com>
2023-02-14 14:23:14 +0500

    Add project files.

---
+++ trackingapi1/Data/IssueDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using trackingapi1.Models;
+
+namespace trackingapi1.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
